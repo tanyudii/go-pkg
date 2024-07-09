e06f8b006b310895935e0bd1467a2446fa5a098f
Yudi Hertanto <y.hertanto17@gmail.com>
2024-07-09 09:48:30 +0700

    [feat] add constructor bad request with field

--- go-err/bad_request.go
+++ go-err/bad_request.go
@@ -71,6 +71,43 @@ func NewBadRequestErrorWithFields(msg string, fields ErrorField) error {
 	}
 }
 
+func NewBadRequestErrorWithCodeAndFields(msg string, code int, fields ErrorField) error {
+	return &BadRequestError{
+		&BaseError{
+			Code:     code,
+			Message:  msg,
+			GRPCCode: badRequestGRPCCode,
+			HTTPCode: badRequestHTTPCode,
+			Fields:   fields,
+		},
+	}
+}
+
+func NewBadRequestErrorWithNameAndFields(msg string, name string, fields ErrorField) error {
+	return &BadRequestError{
+		&BaseError{
+			Name:     name,
+			Message:  msg,
+			GRPCCode: badRequestGRPCCode,
+			HTTPCode: badRequestHTTPCode,
+			Fields:   fields,
+		},
+	}
+}
+
+func NewBadRequestWithCodeNameAndFields(msg string, code int, name string, fields ErrorField) error {
+	return &BadRequestError{
+		&BaseError{
+			Code:     code,
+			Name:     name,
+			Message:  msg,
+			GRPCCode: badRequestGRPCCode,
+			HTTPCode: badRequestHTTPCode,
+			Fields:   fields,
+		},
+	}
+}
+
 func NewBadRequestErrorUsingFieldsOrNil(fields ErrorField) error {
 	if len(fields) == 0 {
 		return nil
